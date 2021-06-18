6eb562995d9c28484647086c7729c31e93a9e78e
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-06-17 23:16:14 -0500

    i'm working in the modal

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const dots = document.querySelectorAll(".dot");
 const sizes = document.querySelectorAll(".sizes__item");
+const buyButton = document.querySelector('#btn-shop');
+const closeModal = document.querySelector('#closeModal');
+const modal = document.querySelector(".modal");
+
 
 function changeFocus(){
    dots.forEach(function(dot){
@@ -14,5 +18,16 @@ function changeSize(){
    this.classList.add('is-selected');
 }
 
+function showModal(){
+   modal.classList.add('isVisible');
+}
+function hiddenModal(e){
+   e.stopPropagation();
+   modal.classList.remove('isVisible');
+}
+
+
 dots.forEach(dot => dot.addEventListener('click', changeFocus))
 sizes.forEach(item=> item.addEventListener('click',changeSize))
+buyButton.addEventListener('click',showModal)
+closeModal.addEventListener('click',hiddenModal)
