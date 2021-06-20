9de853d7c9729b6a14c4fa1b147d557cf62dd605
DavidMarioLC <davidmariolc.dev@gmail.com>
2021-06-19 23:24:25 -0500

    agregando thumbnails

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const sizes = document.querySelectorAll(".sizes__item");
 const buyButton = document.querySelector('#btn-shop');
 const closeModal = document.querySelector('#closeModal');
 const modal = document.querySelector(".modal");
+const thumbnails = document.querySelectorAll('.thumbnail-image');
 
 
 function changeFocus(){
@@ -11,6 +12,7 @@ function changeFocus(){
    })
    this.classList.add('isActive');
 }
+
 function changeSize(){
    sizes.forEach(function(item){
     item.classList.remove('is-selected');
@@ -21,13 +23,19 @@ function changeSize(){
 function showModal(){
    modal.classList.add('isVisible');
 }
-function hiddenModal(e){
-   
+
+function hiddenModal(){
    modal.classList.remove('isVisible');
 }
 
+function changeThumbnail(){
+   thumbnails.forEach(thumbnail =>thumbnail.classList.remove('isActiveThumbnail'))
+  this.classList.toggle('isActiveThumbnail');
+   
+}
 
 dots.forEach(dot => dot.addEventListener('click', changeFocus))
 sizes.forEach(item=> item.addEventListener('click',changeSize))
 buyButton.addEventListener('click',showModal)
 closeModal.addEventListener('click',hiddenModal)
+thumbnails.forEach(thumbnail=>thumbnail.addEventListener('click',changeThumbnail))
